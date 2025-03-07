2644629839cf01359784e4e3c13a387bea55d859
arcgate-saloni <saloni.jain@arcgate.com>
2025-03-07 18:04:01 +0530

    reivew changes and corrected new line error in macros

--- macros/total_msrp_by_legislative_district.sql
+++ macros/total_msrp_by_legislative_district.sql
@@ -1,6 +1,11 @@
 {% macro total_msrp_by_legislative_district(district) %}
-    select "Legislative District", SUM("Base MSRP") AS total_msrp
-    from {{ source('DBT_DB', 'ELECTRIC_VEHICLE') }}
-    where "Legislative District" = '{{ district }}'
-    group by "Legislative District"
+    select 
+        "Legislative District", 
+        SUM("Base MSRP") AS total_msrp
+    from 
+        {{ source('DBT_DB', 'ELECTRIC_VEHICLE') }}
+    where 
+        "Legislative District" = '{{ district }}'
+    group by 
+        "Legislative District"
 {% endmacro %}
